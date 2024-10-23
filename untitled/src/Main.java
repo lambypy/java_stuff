e94ef8ff9d5aab3ffbab25c92f6ceec55a179274
lambypy <danbud@hotmail.co.uk>
2024-10-23 15:02:04 +0100

    CREATE: main initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Task 1 - String Variables
+        String firstName, lastName;
+        firstName = "A-Dan";
+        lastName = "Dan-A";
+
+        String name = firstName + " " + lastName;
+
+        System.out.println(name);
+    }
+}
