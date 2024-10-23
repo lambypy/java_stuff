12a9ce152c76491d5fb546f477ace3f471455033
A01Mlner <milneradam0@gmail.com>
2024-10-23 15:11:50 +0100

    MOD: added some integer calculations

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         // Task 1 - String Variables
         String firstName, lastName;
@@ -8,5 +8,22 @@ public class Main {
         String name = firstName + " " + lastName;
 
         System.out.println(name);
+    }
+}*/
+
+public class Main {
+    public static void main(String[] args) {
+        int num1,num2;
+        num1 = 1;
+        num2 = 2;
+        System.out.println("sum: "+ (1+2));
+        double result = (double)num1/(double)num2;
+
+        int num3 = 8;
+        System.out.println("num3++: "+num3++);
+
+        System.out.println("num3 after: "+num3);
+
+        System.out.println("++num3: "+ ++num3);
     }
 }
