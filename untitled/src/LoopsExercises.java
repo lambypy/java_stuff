f78423972af2e1cb3cf6e0e27b9a6e72516777d8
lambypy <danbud@hotmail.co.uk>
2024-10-24 10:04:11 +0100

    CREATE: LoopsExercises

---
+++ untitled/src/LoopsExercises.java
@@ -0,0 +1,25 @@
+package untitled.src;
+
+
+public class LoopsExercises {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        firstTaskSecondExercise();
+    }
+
+
+    public static void firstTaskSecondExercise(){
+        int x = 10;
+        while (x > 0) {
+            System.out.print(x--);
+            System.out.println();
+        }
+        System.out.println("We have lift off!");
+    }
+
+
+    public static void secondTaskSecondExercise(){
+        
+    }
+}
+
