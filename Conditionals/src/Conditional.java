797a4e1a232ef04ecb1cd1354282b075004b3951
lambypy <danbud@hotmail.co.uk>
2024-10-24 11:35:47 +0100

    Fixed question 1 in Conditional.java and added additional comments

--- Conditionals/src/Conditional.java
+++ Conditionals/src/Conditional.java
@@ -1,14 +1,13 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
-public class Main {
+public class Conditional {
     public static void main(String[] args) {
         //-----TASK 1------
-        //
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        /*
+
         System.out.println("Hello and welcome!");
         Scanner nameRequester1 = new Scanner(System.in);
 
@@ -18,12 +17,33 @@ public class Main {
         System.out.print("Enter number 2: ");
         int num2 = nameRequester2.nextInt();     //input
 
-        System.out.printf("%d", Math.max(num1, num2));  //Compares the values
-        */
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        if (num1 > num2){
+            System.out.println(num1);
+        } else if (num2 > num1){
+            System.out.println(num2);
+        } else {
+            System.out.println("They are equal");
+        }
+
+        System.out.println("Please type in a traffic light colour (green, yellow, red)");
+        Scanner trafficLightRequest = new Scanner(System.in);
+        String trafficLight = trafficLightRequest.nextLine();
+        trafficLight = trafficLight.toLowerCase();
+
+        if (trafficLight.equals("green")){
+            System.out.println("It's green - time to go!");
+        } else if (trafficLight.equals("yellow")){
+            System.out.println("It's yellow - get ready to stop!");
+        } else if (trafficLight.equals("red")){
+            System.out.println("It's red - you cannot go!");
+        } else {
+            System.out.println("I don't recognise this traffic light colour");
+        }
+
+
 
         //-----TASK 2------
-        //
+        /*
         Scanner numRequester1 = new Scanner(System.in);
         System.out.print("Enter number: ");
         int num1 = numRequester1.nextInt();
@@ -37,5 +57,6 @@ public class Main {
             case 4 -> System.out.println("Winter");
             default -> System.out.println("Invalid number");
         }
+        */
     }
 }
